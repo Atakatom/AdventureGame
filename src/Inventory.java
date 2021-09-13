b481e42168fa5e16466fb965c42246522aa32461
Atakan Yontar <atakanyontar@gmail.com>
2021-09-13 13:31:00 +0300

    inventory | done

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,42 +1,62 @@
 public class Inventory {
     boolean water, food, firewood;
-    String weaponName, armorName;
-    int weaponDamage, armorDefence;
+    String weaponName, armourName;
+    int weaponDamage, armourDefence;
 
     public Inventory() {
         water = false;
         food = false;
         firewood = false;
         weaponName = "barehanded";
-        armorName = "naked";
+        armourName = "naked";
         weaponDamage = 0;
-        armorDefence = 0;
+        armourDefence = 0;
     }
 
     public boolean doWeHaveWater() {
         return water;
     }
 
-    public void weGainedWater(){
+    public void weGainedWater() {
         water = true;
-        System.out.println("Water is added to your inventory...")
+        System.out.println("Water is added to your inventory...");
     }
 
     public boolean doWeHaveFood() {
         return food;
     }
 
-    public void weGainedFood(){
+    public void weGainedFood() {
         food = true;
-        System.out.println("Food is added to your inventory...")
+        System.out.println("Food is added to your inventory...");
     }
 
     public boolean doWeHaveFirewood() {
         return firewood;
     }
 
-    public void weGainedFirewood(){
+    public void weGainedFirewood() {
         firewood = true;
-        System.out.println("Firewood is added to your inventory...")
+        System.out.println("Firewood is added to your inventory...");
+    }
+
+    public void newWeapon(String weaponName, int weaponDamage) {
+        this.weaponName = weaponName;
+        this.weaponDamage = weaponDamage;
+    }
+
+    public void weaponStatus() {
+        System.out.println(
+                "Name of your weapon is: " + weaponName + "\n" + "The damage your weapon can hit is: " + weaponDamage);
+    }
+
+    public void newArmour(String armourName, int armourDefence) {
+        this.armourName = armourName;
+        this.armourDefence = armourDefence;
+    }
+
+    public void armourStatus() {
+        System.out.println("Name of your armour is: " + armourName + "\n" + "The damage your armour can absorb is: "
+                + armourDefence);
     }
 }
