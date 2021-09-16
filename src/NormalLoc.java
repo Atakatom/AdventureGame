1b5fdecd4e15a0a6921cc2540254cf60f69b5372
Atakan Yontar <atakanyontar@gmail.com>
2021-09-16 21:56:56 +0300

    NormalLoc | created

---
+++ src/NormalLoc.java
@@ -0,0 +1,11 @@
+public class NormalLoc extends Location {
+    @Override
+    public boolean onLocation() {
+        return true;
+    }
+
+    @Override
+    void location() {
+        // TODO Auto-generated method stub
+    }
+}
