a2bf6db6e4d68b37b9ba72fa82cfd5d36f065111
Atakan Yontar <atakanyontar@gmail.com>
2021-09-24 09:37:41 +0300

    BattleLoc & Obstacle | completed

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,3 +1,35 @@
 public class Obstacle {
-    int obstacleNumber;
+    private int id;
+    private int damage;
+    private int health;
+
+    public Obstacle(int id, int damage, int health) {
+        this.id = id;
+        this.damage = damage;
+        this.health = health;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
 }
