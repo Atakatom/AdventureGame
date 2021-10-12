ae2485d653a6abc5a5b7e52f3bf9f3d6155b45a0
Atakan Yontar <atakanyontar@gmail.com>
2021-10-12 15:17:07 +0300

    all battle fields are done

--- src/Forest.java
+++ src/Forest.java
@@ -1,11 +1,13 @@
-// public class Forest extends BattleLoc {
-// public Forest() {
-// super(new Vampire());
-// }
+public class Forest extends BattleLoc {
+    static int monsterID = 0;
+    static {
+        for (int i = 0; i < Obstacle.getMonsters().size(); i++) {
+            if (Obstacle.getMonsters().get(i).getName().toLowerCase().equals("vampire"))
+                monsterID = i;
+        }
+    }
 
-// @Override
-// void location() {
-// // TODO Auto-generated method stub
-
-// }
-// }
+    public Forest(Player player) {
+        super(player, "Forest", Obstacle.getMonsters().get(monsterID));
+    }
+}
--- src/River.java
+++ src/River.java
@@ -1,11 +1,13 @@
-// public class River extends BattleLoc {
-// public River() {
-// super(new Bear());
-// }
+public class River extends BattleLoc {
+    static int monsterID = 0;
+    static {
+        for (int i = 0; i < Obstacle.getMonsters().size(); i++) {
+            if (Obstacle.getMonsters().get(i).getName().toLowerCase().equals("bear"))
+                monsterID = i;
+        }
+    }
 
-// @Override
-// void location() {
-// // TODO Auto-generated method stub
-
-// }
-// }
+    public River(Player player) {
+        super(player, "River", Obstacle.getMonsters().get(monsterID));
+    }
+}
